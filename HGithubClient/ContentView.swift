346af93eb75c1edb132a15dc8e5b68965fabba18
Bell <dienbell@gmail.com>
2024-11-20 17:50:11 +0800

    Initial Commit

---
+++ HGithubClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HGithubClient
+//
+//  Created by Bell Dien on 2024/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HGithubClient/HGithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  HGithubClientApp.swift
+//  HGithubClient
+//
+//  Created by Bell Dien on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct HGithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
